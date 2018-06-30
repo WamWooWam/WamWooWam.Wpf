6ebf6b3a21dd4989a8cb7085fdba30aeedec8ce7
Thomas May <wamwoowam@gmail.com>
2018-06-30 17:33:01 +0100

    Changes to ScrollViewer
    It actually applies to shit now

---
+++ WamWooWam.Wpf/Themes/ScrollBars.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Interop;
+using System.Windows.Media;
+
+namespace WamWooWam.Wpf.Theme
+{
+    partial class ScrollBars : ResourceDictionary
+    {
+        public ScrollBars()
+        {
+            InitializeComponent();
+        }
+
+        private void ScrollViewer_ScrollChanged(object sender, ScrollChangedEventArgs e)
+        {
+
+        }
+    }
+}
